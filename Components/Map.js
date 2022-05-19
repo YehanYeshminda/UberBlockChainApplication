656edcd544150e264f351a682e87ea5eb64ca632
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2022-05-19 18:25:08 +0530

    added the map

---
+++ Components/Map.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+// import { UberContext } from '../context/uberContext';
+
+const style = {
+	wrapper: `flex-1 h-full w-full`,
+};
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+
+const Map = () => {
+	// const { pickupCoordinates, dropoffCoordinates } = useContext(UberContext);
+
+	useEffect(() => {
+		const map = new mapboxgl.Map({
+			container: 'map',
+			style: 'mapbox://styles/yehannnn/cl35elovv000514qn850arl7g',
+			center: [82.026177, 7.577576],
+			zoom: 6.7,
+		});
+	}, [,]);
+
+	return <div className={style.wrapper} id="map" />;
+};
+
+export default Map;
